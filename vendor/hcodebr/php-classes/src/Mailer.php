6f564397432129008e0c41c0ec4d1120c4df2e9d
lazaroconceicao <42076241+lazaroconceicao@users.noreply.github.com>
2019-06-28 10:05:29 -0300

    Update Mailer.php
    Tirado email e senha

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -6,8 +6,8 @@ use Rain\Tpl;
 
 class Mailer {
 
-	const USERNAME = "joselazaroc@gmail.com";
-	const PASSWORD = "!Lazinho@1";
+	const USERNAME = "xxxxxxxxx";
+	const PASSWORD = "xxxxxxx";
 	const NAME_FROM = "Hcode Store";
 
 	private $mail;
